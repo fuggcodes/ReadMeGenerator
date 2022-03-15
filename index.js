fecb5c3ac2ed01e4f04c7aeaaf37e7c69f29ff2e
David Binyatov <davidbphotography101@gmail.com>
2022-03-15 15:45:34 -0400

    built the array of questions

--- index.js
+++ index.js
@@ -5,13 +5,62 @@ var fs = require("fs");
 const path = require("path");
 const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "github",
+        message: "What's your username on github",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: `What kind of license does your project have`,
+        choices: ["MIT", "GNU GPLv3", "Apache-2.0", "ISC"]
+    },
+    {
+        type: "input",
+        name: "email",
+        message: `What's your email address?`,
+    },
+    {
+        type: "input",
+        name: "title",
+        message: `What is your project's name?`,
+    },
+    {
+        type: "input",
+        name: "descriptions",
+        message: `describe your project?`,
+    },
+
+    {
+        type: "input",
+        message: `How can someone install your program?`,
+        name: "install",
+    },
+    
+    {
+        type: "input",
+        message: `What is this project used for?`,
+        name: "usage",
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: `Who's a contributor for this project?`,
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "what are you testing?",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
